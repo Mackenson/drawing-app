6f0615c7f39aae18d2f3a58e0565d4d048c81b7c
Mackenson <mackensondorancy@yahoo.com>
2018-05-14 13:43:41 -0400

    Painting index pages with react

--- app/javascript/react/components/PaintInfo.js
+++ app/javascript/react/components/PaintInfo.js
@@ -3,10 +3,13 @@ import React from 'react';
 const PaintInfo = (props) => {
   console.log(props);
   return (
+    <div>
+      <div id="header-bckg"></div>
     <div id="photo-box">
       <h3 className="box-title">{props.title}</h3>
       <img src={props.photo} />
     </div>
+  </div>
   );
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # namespace :api do
-  #     namespace :v1 do
-  #       resources :paints do
-  #       end
-  #     end
-  #   end
+  namespace :api do
+      namespace :v1 do
+        resources :paints do
+        end
+      end
+    end
 
     resources :paints, only: [:index, :new, :create,:update, :edit, :show, :destroy] do
       resources :reviews, only: [:new, :create]
