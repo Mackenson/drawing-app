3136726087e6cba081e573b8d709922195009927
Mackenson <mackensondorancy@yahoo.com>
2018-05-17 14:24:50 -0400

    styling in general

--- app/javascript/react/components/PaintInfo.js
+++ app/javascript/react/components/PaintInfo.js
@@ -5,14 +5,15 @@ const PaintInfo = (props) => {
   console.log(props);
   return (
     <div>
-      <div id="header-bckg"></div>
-    <div id="photo-box">
-      <h3 className="box-title">{props.title}</h3>
-      <img src={props.photo} />
-    <button><Link to={`/paints/${props.id}`} >
-    Add Review
-  </Link></button>
-    </div>
+      <div id="photo-box">
+        <h3 className="box-title">{props.title}</h3>
+        <img src={props.photo} />
+        <button>
+          <Link to={`/paints/${props.id}`} >
+          Add Review
+          </Link>
+        </button>
+      </div>
   </div>
   );
 }
