c271b727d23357eb5cb250236f9d44f36e249956
Mackenson <mackensondorancy@yahoo.com>
2018-05-18 15:49:03 -0400

    fix registration spec

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -16,6 +16,8 @@ feature 'user registers', %Q{
     visit new_user_registration_path
 
     fill_in 'Email', with: 'john@example.com'
+    fill_in 'First Name', with: 'Mackenson'
+    fill_in 'Last Name', with: 'Dorancy'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
 
