2237ab275e5e0c7dba6b6d5d6410f0b505cb0c33
Mackenson <mackensondorancy@yahoo.com>
2018-07-13 11:03:59 -0400

    I made some changing in the meetup api

--- app/javascript/react/components/Meets.js
+++ app/javascript/react/components/Meets.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Meets = (props) => {
   return (
-    <div id="meet-hide" onClick={props.click}>
+    <li onClick={props.click}>
       <h3>{props.name}</h3>
-    </div>
+    </li>
   );
 }
 
